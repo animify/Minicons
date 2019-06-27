3705017d03e0280a192bea1ba1a53ebec5236135
Stefan Mansson <stefan.aotik@gmail.com>
2019-06-27 20:07:14 +0300

    chore: refactor to umd packager

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -2,11 +2,9 @@ import path from 'path';
 import MinifyPlugin from 'babel-minify-webpack-plugin';
 
 export default {
-    entry: [
-        path.resolve(__dirname, 'src'),
-    ],
+    entry: [path.resolve(__dirname, 'src')],
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'umd'),
         libraryTarget: 'umd',
         library: 'minicons',
         filename: 'minicons.min.js',
@@ -20,7 +18,5 @@ export default {
             },
         ],
     },
-    plugins: [
-        new MinifyPlugin()
-    ]
+    plugins: [new MinifyPlugin()],
 };
